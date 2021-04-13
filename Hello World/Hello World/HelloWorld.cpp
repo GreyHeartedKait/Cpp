8bb6a30141cddce5df1fc2977ee14598955f8bda
GreyHeartedEliza <bui.kai.eliza@gmail.com>
2021-04-12 17:11:41 -0700

    Editted HelloWorld Source file

--- Hello World/Hello World/HelloWorld.cpp
+++ Hello World/Hello World/HelloWorld.cpp
@@ -1,1 +1,15 @@
-world
+/*
+Program: HelloWorld
+Purpose: This program is my first program in C++. 
+Author: Kaitlyn Bui
+Date: 04.12.2021
+*/
+
+#include <iostream> //Standard header 
+using namespace std; //std=standard //All complete statements in C++ need headers 
+
+int main(){//int main() is the header for the function, function body is contained within the curly braces 
+	cout << "Hello World!" << endl;
+
+	system("pause"); //tells system to pause output window 
+}
