5bfe51471acbbcf60942cd744cd9f1e4b981bcb2
GreyHeartedEliza <bui.kai.eliza@gmail.com>
2021-04-15 14:49:57 -0700

    Added source code for "Pizza Pi"

--- Pizza Pi/enc_temp_folder/941ef3224aec6fa8b1391a49646322bc/Pizza Pi.cpp
+++ Pizza Pi/enc_temp_folder/941ef3224aec6fa8b1391a49646322bc/Pizza Pi.cpp
@@ -1,21 +1,9 @@
-/*
-* Author: Kaitlyn Bui
-* Date: 04.15.2021
-* Program: Pizza Pi
-* Purpose: Calculates the number of slices a pizza can have.
-*/
 #include<cmath>
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	/*
-	* var double diameter: the diameter of the pizza
-	* var double area: the area of the pizza
-	* const double PI: the value pi
-	* const double SLIVE_SIZE: the area of each slice 
-	*/
 	const double PI = 3.1415926535897932384626433832795028841;
 	const double SLICE_SIZE = 14.125;
 	double diameter, area;
