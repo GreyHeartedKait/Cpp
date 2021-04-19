3015256f0ce7467ecab38441b773727350ed8828
GreyHeartedEliza <bui.kai.eliza@gmail.com>
2021-04-19 11:55:17 -0700

    Added project quiz 8

---
+++ Quiz 8/Quiz 8/Quiz 8.cpp
@@ -0,0 +1,47 @@
+/*
+* Author: kaitlyn Bui
+* Date: 04.19.2021
+* Program: Quiz 8
+* Purpose: Calculate a BMI and give a weight range.
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+/*
+* Purpose: Calculate BMI
+* var double weight: inputted weight
+* var double height: imputted height
+*/
+double bmiCalc(double weight, double height){
+	return weight * (703 / pow(height, 2));
+}
+
+int main(){
+	double weight, height, BMI;
+	cout << "Hello user, welcome to the BMI calculator. Here, you will be able to calculate\n"
+		<< "your BMI and see your weight range."
+		<< "\n\nPlease enter your weight: ";
+
+	cin >> weight;
+
+	cout << "Please enter your height: ";
+	cin >> height;
+	BMI = bmiCalc(weight, height);
+
+	cout << endl
+		<< "Your BMI is: " << BMI
+		<< "\nYour weight range is: ";
+
+	if(18.5<=BMI && BMI<=25){
+		cout << "Optimal";
+	}
+
+	else if (BMI < 18.5) {
+		cout << "Underweight";
+	}
+
+	else {
+		cout << "Overweight";
+	}
+}
