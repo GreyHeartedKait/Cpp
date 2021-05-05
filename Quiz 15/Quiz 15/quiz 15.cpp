6f27a847ff25be1e9b360128f7a0d9ae0b606f6f
GreyHeartedKait <bui.kai.eliza@gmail.com>
2021-05-05 01:51:37 -0700

    .

---
+++ Quiz 15/Quiz 15/quiz 15.cpp
@@ -0,0 +1,22 @@
+/*
+* Author: Kaitlyn Bui
+* Date: 05.03.2021
+* Program: Quiz 15
+* Purpose: Reads input files, processes, and outputs information
+*/
+#include <iostream>
+#include <fstream>
+	
+using namespace std;
+
+int main() {
+	ifstream iFile("C:\\Users\\User\\source\\repos\\C++ Class Repos\\Tester\\Tester\\Friends.txt");
+	string foo;
+
+	for (int i =1; i <= 3; i++) {	//prints out three names of friends 
+		iFile >> foo;
+		cout << "Friend #" << i << ": " << foo << endl;
+	}
+
+	system("pause");
+}
